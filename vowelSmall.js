2779b6f6291fd8d7b0d56cc3c6284dc6ed0d121e
jiteshcelsys <Jitesh.p@celestialsys.com>
2023-02-24 18:10:21 +0530

    add

---
+++ vowelSmall.js
@@ -0,0 +1,13 @@
+const str = 'DECISION!! HURRAH';
+const vowel = 'AEIOU';
+let newStr = '';
+
+for (let i = 0, l = str.length; i < l; i++) {
+  if (vowel.includes(str[i - 1])) {
+    newStr =`${newStr}${str[i].toLowerCase()}`;
+  } else {
+    newStr = `${newStr}${str[i]}`;
+  }
+}
+
+console.log(newStr);
