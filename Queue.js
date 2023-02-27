e08718b64a515f49402f578b1947a29acc01a5eb
jiteshcelsys <Jitesh.p@celestialsys.com>
2023-02-27 14:29:26 +0530

    stack_queue

---
+++ Queue.js
@@ -0,0 +1,66 @@
+let queue = [];
+const maxSize = 5;
+let currentSize = queue.length
+
+function addQueue(newVal){
+  if(currentSize >= maxSize){
+    console.log('limit reached')
+  }
+    else{
+      queue[currentSize] = newVal;
+      currentSize ++;
+    }
+
+}
+function removeQueue(){
+  if(currentSize>0){
+    for(let i=0; i<queue.length; i++){
+      queue[i] = queue[i+1];
+    }
+    currentSize --;
+    queue.length = currentSize;
+  }
+  else{
+    console.log('queue is empty')
+  }
+  
+}
+addQueue(18)
+addQueue(19)
+removeQueue();
+console.log(queue);
+
+// removeQueue();
+
+
+// let queue = [];
+// let maxSize = 5;
+// let currentSize = queue.length;
+
+// function QueueAdd(newVal) {
+//   if (currentSize >= maxSize) {
+//     console.log("queue length exceeded");
+//   } else {
+//     queue[currentSize] = newVal;
+//     currentSize++;
+//   }
+// }
+
+// function deQueue() {
+//   if (currentSize > 0) {
+//     for (let i = 0; i < queue.length; i++) {
+//       queue[i] = queue[i + 1];
+//     }
+//     currentSize--;
+//     queue.length = currentSize;
+//   } else {
+//     console.log("queue is empty");
+//   }
+// }
+// QueueAdd(10);
+// QueueAdd(20);
+// QueueAdd(30);
+// QueueAdd(40);
+
+// deQueue();
+// console.log(queue);
---
+++ Stack.js
@@ -0,0 +1,30 @@
+let arr = [];
+let currentSize = arr.length;
+const maxSize = 5;
+
+function push(newVal) {
+  if (currentSize >= maxSize) {
+    console.log("Max size" + newVal);
+  } else {
+    arr[currentSize] = newVal;
+    currentSize += 1;
+  }
+}
+function pop() {
+  if (currentSize > 0) {
+    currentSize += -1;
+    arr.length = currentSize;
+  }
+  console.log("all elements is removed from an array");
+}
+console.log(currentSize);
+push(5);
+push(4);
+push(52);
+push(58);
+push(5);
+push(35);
+push(775);
+pop();
+pop();
+console.log(arr);
