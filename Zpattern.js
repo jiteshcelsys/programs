1e162a90a3988b4e4853d35ce17f4efa7aadaf8d
jiteshcelsys <Jitesh.p@celestialsys.com>
2023-03-01 17:08:52 +0530

    zpattern

--- Zpattern.js
+++ Zpattern.js
@@ -2,7 +2,7 @@ function ZPattern(num) {
   let result = '';
   for (let i = 1; i <= num; i++) {
     for (let j = 1; j <= num; j++) {
-      if ((i == 1||i == num) || (i + j == num + 1)) {
+      if ((i == 1 || i == num) || (i + j == num + 1)) {
         result += '*';
       } else {
         result += ' ';
