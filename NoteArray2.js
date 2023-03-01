61274e66353768d2658ebca1f1696da25af50bd3
jiteshcelsys <Jitesh.p@celestialsys.com>
2023-03-01 12:27:51 +0530

    add

--- NoteArray2.js
+++ NoteArray2.js
@@ -1,16 +1,16 @@
-const note=[50,10,5,1];
+const note = [50, 10, 5, 1];
 
-function getNotes( note, amount){
+function getNotes(note, amount) {
   let result = {};
   let reminder = amount;
-  for (let i = 0; i < note.length; i++) {
-   if(reminder !== 0){
-    let quotient = reminder/note[i]
-    result[note[i]] = parseInt(quotient)
-    reminder = amount % note[i];
-   }
+  for (let i = 0, l = note.length; i < l; i++) {
+    if (reminder !== 0) {
+      let quotient = reminder / note[i];
+      result[note[i]] = parseInt(quotient);
+      reminder = amount % note[i];
+    }
   }
   console.log(result);
 }
 
-getNotes(note, 121)
+getNotes(note, 101);
